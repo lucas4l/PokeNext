3f5ae2128a763e375a461300a33b8fe9e3a23a61
lucas4l <4lendarios>
2023-01-07 14:42:09 -0300

    listando pokemon fora do estipulado

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -3,6 +3,8 @@ import Image from "next/image"
 
 import styles from "../../styles/Pokemon.module.css"
 
+import { useRouter } from "next/router"
+
 
 export const getStaticPaths = async() => {
 
@@ -20,7 +22,7 @@ export const getStaticPaths = async() => {
 
   return{
     paths,
-    fallback: false,
+    fallback: true,
   }
 
 }
@@ -40,6 +42,13 @@ export const getStaticProps = async(context) => {
 }
 
 const Pokemon = ({pokemon}) => {
+
+  const router= useRouter()
+
+  if(router.isFallback){
+    return <div>Carregando...</div>
+  }
+
 
   return(
     <div className={styles.pokemon_container}>
