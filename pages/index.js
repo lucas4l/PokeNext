6a0e3014be1411829381f568c6ffc0509d5492b0
lucas4l <barbosa-lucas2@hotmaillcom>
2023-02-22 20:38:51 -0300

    251 pokemons

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Card from '../components/Card'
 
 export async function getStaticProps(){
 
-  const maxPokemons = 250
+  const maxPokemons = 251
   const api = 'https://pokeapi.co/api/v2/pokemon/'
 
   const res = await fetch(`${api}/?limit=${maxPokemons}`)
